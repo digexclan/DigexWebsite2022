91d3882370b6f0e4a3cd489f71038a2676ee0c2d
Malonic <97789166+Malonic@users.noreply.github.com>
2022-07-04 00:02:11 +0530

    Add files via upload

---
+++ components/footer.tsx
@@ -0,0 +1,26 @@
+import { faFacebook, faInstagram, faLinkedin, faMailchimp, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import Link from 'next/link';
+
+function footer() {
+  return (
+    <div className='flex flex-row justify-center items-center mt-16'>
+      <div className='flex flex-col items-center justify-center py-8 border-t-2 w-4/6'>
+        <div>
+          <FontAwesomeIcon icon={faFacebook} className='mr-2' style={{width: 22, height: 22}}/>
+          <FontAwesomeIcon icon={faInstagram} className='mr-2' style={{width: 22, height: 22}}/>
+          <FontAwesomeIcon icon={faLinkedin} className='mr-2' style={{width: 22, height: 22}}/>
+          <FontAwesomeIcon icon={faYoutube} style={{width: 22, height: 22}}/>
+        </div>
+        <div className='flex flex-col items-center justify-center'>
+          <h1 className='text-xl'>Digexclan</h1>
+          <Link href="mailto: digex@dpsrkp.net">digex@dpsrkp.net</Link>
+          <p>&copy;2022-23 Digexclan</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default footer;
